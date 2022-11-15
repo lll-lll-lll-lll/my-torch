8ae1861d0b35044974ba90b9ddbeb134d873b7c1
0xpeIpeI <hokumaru12@gmail.com>
2022-11-15 17:48:28 +0900

    モデルの移動

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
 import torch
-import torchvision
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+from datasets.hellworld import CustomFashionMNIST
 
 from network.hellworld import CustomNeuralNetwork
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
-class CustomFashionMNIST:
-    BATCH_SIZE = 64
-    def __init__(self):
-        pass
-
-    def get_training_data(self)-> torchvision.datasets.FashionMNIST:
-        training_data = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor()
-        )
-        return training_data
-    
-    def get_test_data(self) -> torchvision.datasets.FashionMNIST:
-        test_data = datasets.FashionMNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor()
-        )
-        return test_data   
-    
-    def creatre_dataloader(self, data):
-        return DataLoader(data, batch_size=self.BATCH_SIZE)
-    
-    def x_shape_y_shape_dtypes(self, dataloader:DataLoader):
-        for X, y in dataloader:
-            print(f"Shape of X [N, C, H, W]: {X.shape}")
-            print(f"Shape of y: {y.shape} {y.dtype}")
-            break
-
-
-
     
 if __name__ == "__main__":
     customFashion = CustomFashionMNIST()
@@ -53,24 +14,4 @@ if __name__ == "__main__":
     model.exec(5,train_loader=train_dataloader, test_loader=test_dataloader)
     for acc, avg_loss in zip(model.acc, model.avg_loss):
         print("Accuracy: ",acc, "AVG: ", avg_loss)
-    # model.save_model("test_model")
-    # model = CustomNeuralNetwork(device)
-    # model.load_state_dict(torch.load("test_model.pth"))
-    # classes = [
-    # "T-shirt/top",
-    # "Trouser",
-    # "Pullover",
-    # "Dress",
-    # "Coat",
-    # "Sandal",
-    # "Shirt",
-    # "Sneaker",
-    # "Bag",
-    # "Ankle boot",
-    # ]
-    # model.eval()
-    # x, y = test_data[0][0], test_data[0][1]
-    # with torch.no_grad():
-    #     pred = model(x)
-    #     predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    #     print(f'Predicted: "{predicted}", Actual: "{actual}"')
+    
